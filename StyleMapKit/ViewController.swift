eb1ca5461d7a63d92e0df3375ea228cafa3026d1
Jules Lee <jackconsolidated@gmail.com>
2019-10-04 21:23:02 +0800

    Update ViewController.swift

--- StyleMapKit/ViewController.swift
+++ StyleMapKit/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
+import MapKitGoogleStyler
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        guard let overlayFileURLString = Bundle.main.path(forResource: "overlay", ofType: "json") else {
+                return
+        }
+        let overlayFileURL = URL(fileURLWithPath: overlayFileURLString)
+        
+        // After that, you can create the tile overlay using MapKitGoogleStyler
+        guard let tileOverlay = try? MapKitGoogleStyler.buildOverlay(with: overlayFileURL) else {
+            return
+        }
+        
+        // And finally add it to your MKMapView
+        mapView.addOverlay(tileOverlay)
     }
 
-
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            // This is the final step. This code can be copied and pasted into your project
+            // without thinking on it so much. It simply instantiates a MKTileOverlayRenderer
+            // for displaying the tile overlay.
+            if let tileOverlay = overlay as? MKTileOverlay {
+                return MKTileOverlayRenderer(tileOverlay: tileOverlay)
+            } else {
+                return MKOverlayRenderer(overlay: overlay)
+            }
+    }
 }
-
